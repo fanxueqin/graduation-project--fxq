b6cb5a83ae1338feed22337da5b803202caa17aa
fanxueqin <fanxueqin>
2017-03-22 20:36:44 +0800

    fourth commit 2017-3-22

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -74,4 +74,45 @@ angular
               }
             }
           })
+          .state("shop_hufu",{
+            url:"/shop_hufu",
+            views: {
+              '':{
+                templateUrl: "views/shop_hufu.html"
+              }
+            }
+          })
+          .state("shop_makeup",{
+            url:"/shop_makeup",
+            views: {
+              '':{
+                templateUrl: "views/shop_makeup.html"
+              }
+            }
+          })
+          .state("shop_fashion",{
+            url:"/shop_fashion",
+            views: {
+              '':{
+                templateUrl: "views/shop_fashion.html"
+              }
+            }
+          })
+          .state("shop_accesory",{
+            url:"/shop_accesory",
+            views: {
+              '':{
+                templateUrl: "views/shop_accesory.html"
+              }
+            }
+          })
+          .state("shop_home",{
+            url:"/shop_home",
+            views: {
+              '':{
+                templateUrl: "views/shop_home.html"
+              }
+            }
+          })
+
   });
